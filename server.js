23e8ece4f32994647aedb3db381f7ac6aaa44541
Gnoah <root@localhost.localdomain>
2019-05-08 13:48:33 +0300

    liaison

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// Configuring the database
+const dbConfig = require('./config/database.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+require('./route/route.js')(app);
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("connexion avec succes");    
+}).catch(err => {
+    console.log('erreur de connexion', err);
+    process.exit();
+});
+
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to simplon."});
+});
+
+app.listen(8080, () => {
+    console.log("Server demarer 8080");
+});
